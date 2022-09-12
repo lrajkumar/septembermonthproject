fdef257b707b0672d73d3956e10e7c0f033c239f
Raja Asthana <rajaliving@gmail.com>
2022-09-12 12:51:22 +0530

    First Project commit in sep month

---
+++ Exception Handling/src/Exceptiontutorials/ExcepTutorial.java
@@ -0,0 +1,15 @@
+package Exceptiontutorials;
+
+public class ExcepTutorial {
+	public static void main (String[] args) {
+		int i=50;
+		int j=0;
+		try {
+			System.out.println(i/j);
+		}
+		catch (Exception e) {
+			System.out.println("run code");
+		}
+	}
+
+}
